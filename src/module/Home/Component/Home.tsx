b6d198bdb2626858e732a26fb8c9b399eb529754
polar_foxx <suchovsky.michal@gmail.com>
2023-04-25 15:19:44 +0200

    dizajn_teacher_and_layout

--- src/module/Home/Component/Home.tsx
+++ src/module/Home/Component/Home.tsx
@@ -6,6 +6,7 @@ function Home(): JSX.Element {
         <div className="Home">
               <div className="homeTxt">
                     <p>This site is intended for teachers of auxiliary schools. For the registration of problem pupils in primary and secondary institutions</p>
+                    <p>We ask all users to comply with the rules according to the applicable laws on privacy protection.. The administrator of the site is not responsible for the leakage of information..!</p>
               </div>
               <div className="homeImg">
                     <img src="../../../../img/teacher.png" alt="" />
--- src/module/Teacher/Component/Teacher.tsx
+++ src/module/Teacher/Component/Teacher.tsx
@@ -7,10 +7,10 @@ function Teacher(): JSX.Element {
         <div className="Teacher">
                 <div className='teacherContext-link-container'>
                     <div className="teacherlink-block">
-                        < Link className="Tlink " to="/Teacher/CreateTeacher">CreateTeacher</Link>
-                        < Link className="Tlink " to="/Teacher/TeacherALL">TeacherALL</Link>
-                        < Link className="Tlink " to="/Teacher/TeachersByID">TeachersByID</Link>
-                        < Link className="Tlink " to="/Teacher/UpdateTeacher">UpdateTeacher</Link>
+                        < Link className="Tlink " to="/Teacher/CreateTeacher">Create teacher</Link>
+                        < Link className="Tlink " to="/Teacher/TeacherALL">Teacher</Link>
+                        < Link className="Tlink " to="/Teacher/TeachersByID">Teachers by ID</Link>
+                        < Link className="Tlink " to="/Teacher/UpdateTeacher">Update teacher</Link>
                         <Outlet />
                 </div>
             </div>
