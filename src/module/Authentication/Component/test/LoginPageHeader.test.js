f4c86577bd8b132ba3a1131313ea9bc5a66e379c
polar_foxx <suchovsky.michal@gmail.com>
2023-05-09 17:14:38 +0200

    first_test

---
+++ src/module/Authentication/Component/test/LoginPageHeader.test.js
@@ -0,0 +1,11 @@
+import { render, fireEvent, screen } from "@testing-library/react";
+import LoginPageHeader from "../LoginPageHeader";
+import "./setupTest"
+
+
+test("testing LoginPage", () => {
+    render(<LoginPageHeader />)
+
+    const element = screen.getByText("Welcome to teacher and students databaze");
+    expect(element).toBeInTheDocument()
+})
